b29f1a854682ee479d19c998aa3fc486198d40ec
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-12 18:14:49 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root to: "messages#index"
-  
+  # get 'messages/index'
+  root to: "rooms#index"
+
   resources :users, only: [:edit, :update]
   # 新規チャットルーム作成で動くアクションはnew, createのみ
   resources :rooms, only: [:new, :create] 
