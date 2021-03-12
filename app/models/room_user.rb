df09ab4ad508f3b14312e81bcc179d60d25e92d3
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-12 16:33:10 +0900

    プルダウンの実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,6 @@
 class RoomUser < ApplicationRecord
   belongs_to :room
   belongs_to :user
+   # Room.create(name: "ルーム１", user_ids: [1, 2])とコンソールで出力し、テーブルに追加。モデル名+_idsで所属させていものを配列で指定すると、中間テーブルに保存される。
+
 end
