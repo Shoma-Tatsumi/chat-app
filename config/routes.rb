1b8f3f37f13ff101a77f0b9f63ffec766b24e27b
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-12 14:15:27 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
+  
   resources :users, only: [:edit, :update]
+  # 新規チャットルーム作成で動くアクションはnew, createのみ
+  resources :rooms, only: [:new, :create] 
 end
