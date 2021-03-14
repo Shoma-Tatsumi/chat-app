de3aced3d38f7ddc22548c35a59d52735b171675
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-03-14 12:25:40 +0900

    Revert "画像のみでもメッセージ送信可能となるバリデーションの設定"
    This reverts commit d2b4772eeccc0b0fce71509c1c4b653fd03d8d75.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,10 +3,5 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content ,presence: true, unless: :was_attached? #「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しない
-                                       #↑validatesのunlessオプションにメソッド名を指定することで、「メソッドの返り値がfalseならばバリデーションによる検証を行う」という条件を作っています。
-                                       #指定されたwas_attached?メソッドは、self.image.attached?という記述によって、画像があればtrue、なければfalseを返す仕組み
-  def was_attached?
-    self.image.attached?
-  end
+  validates :content ,presence: true #「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しない
 end
